7b6b3074d4b89b53a5d82367812850fad76ea1ee
G. D. McBain <gdmcbain@protonmail.com>
2017-08-01 21:37:40 +1000

    managed to print a Hash record to stdout

--- haskell/hakecoin/src/Lib.hs
+++ haskell/hakecoin/src/Lib.hs
@@ -17,11 +17,27 @@ data Hash = Hash { index :: Int
                  , hash :: String
                  } deriving (Show)
 
+makeHash :: Int -> String -> String -> String -> IO Hash
+makeHash n c p h = do
+  now <- getCurrentTime
+  return Hash { index=n
+              , timestamp=now
+              , content=c
+              , previousHash=p
+              , hash=h
+              }
+
+genesisHash :: IO Hash
+genesisHash = makeHash 0 "Genesis Block" "0" "0"
+
 hashTest = h1 where h = SHA256.init
                     h1 = SHA256.update h "someFunc"
 
 hexHash :: SHA256.Ctx -> String
 hexHash = concatMap (printf "%02x") . B.unpack . SHA256.finalize
 
+someFunc0 :: IO ()
+someFunc0 = putStrLn $ hexHash hashTest
+
 someFunc :: IO ()
-someFunc = putStrLn $ hexHash hashTest
+someFunc = (show <$> genesisHash) >>= putStrLn
