a2046a59a1a797853219a192685f090395be4309
G. D. McBain <gdmcbain@protonmail.com>
2017-08-01 21:11:34 +1000

    started to add a Hash record using datetime:Data.DateTime.DateTime

--- haskell/hakecoin/src/Lib.hs
+++ haskell/hakecoin/src/Lib.hs
@@ -2,17 +2,26 @@
 
 module Lib
     ( someFunc
+    , Hash
     ) where
 
 import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.ByteString as B
 import Text.Printf (printf)
+import Data.DateTime (DateTime, getCurrentTime, toSqlString)
 
-hash = h1 where h = SHA256.init
-                h1 = SHA256.update h "someFunc"
+data Hash = Hash { index :: Int
+                 , timestamp :: DateTime
+                 , content :: String -- "data" in Snakecoin
+                 , previousHash :: String
+                 , hash :: String
+                 } deriving (Show)
+
+hashTest = h1 where h = SHA256.init
+                    h1 = SHA256.update h "someFunc"
 
 hexHash :: SHA256.Ctx -> String
 hexHash = concatMap (printf "%02x") . B.unpack . SHA256.finalize
 
 someFunc :: IO ()
-someFunc = putStrLn $ hexHash hash
+someFunc = putStrLn $ hexHash hashTest
