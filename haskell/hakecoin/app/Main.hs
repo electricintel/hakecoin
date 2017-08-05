a4a7b1f2383b40deef12f540a01d07e191aefd87
G. D. McBain <gdmcbain@protonmail.com>
2017-08-05 12:58:21 +1000

    started thinking about how to iterate

--- haskell/hakecoin/app/Main.hs
+++ haskell/hakecoin/app/Main.hs
@@ -1,9 +1,22 @@
 module Main where
 
-import Lib
+import Control.Monad.Loops (iterateM_)
 
+
+import Lib
 main :: IO ()
 main = do
   h0 <- genesisHash
   print h0
-  nextHash "" h0 >>= print
+  h1 <- nextHash "" h0
+  print h1
+
+  -- How to apply iterate :: (a -> a) -> a -> [a].
+
+  -- We have:
+
+  -- (take 20 $ iterate ((nextHash "") =<<) genesisHash) :: [IO Hash]
+
+  -- mapM_ print :: (Show a, Foldable t) => t a -> IO ()
+
+  -- so with t = [], a = Hash that's [Hash] -> IO ()
